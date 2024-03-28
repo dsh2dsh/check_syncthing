37ebd2022d70ba63b59a48d1c953310ae584e073
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2024-03-28 20:44:12 +0100

    Add more tests

--- client/client_test.go
+++ client/client_test.go
@@ -181,16 +181,12 @@ func TestCache_Health(t *testing.T) {
 }
 
 func TestCache_Connections(t *testing.T) {
-	const conn1 = "XXXXXX1-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXX1"
-	const conn2 = "XXXXXX2-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXX2"
-
 	tests := []struct {
 		name        string
 		statusCode  int
 		contentType string
 		body        string
 		assertErr   func(t *testing.T, err error)
-		assert      func(t *testing.T, conn *api.Connections)
 	}{
 		{
 			name:        "OK",
@@ -253,15 +249,6 @@ func TestCache_Connections(t *testing.T) {
     "outBytesTotal": 427863355
   }
 }`,
-			assert: func(t *testing.T, conn *api.Connections) {
-				require.NotEmpty(t, conn.Connections)
-				assert.Len(t, conn.Connections, 2)
-				assert.Contains(t, conn.Connections, conn1)
-				assert.NotZero(t, conn.Connections[conn1])
-				assert.Contains(t, conn.Connections, conn2)
-				assert.Zero(t, conn.Connections[conn2])
-				assert.NotZero(t, conn.Total)
-			},
 		},
 		{
 			name:        "empty body",
@@ -310,9 +297,640 @@ func TestCache_Connections(t *testing.T) {
 				tt.assertErr(t, err)
 			} else {
 				require.NoError(t, err)
-				require.NotNil(t, conn)
-				if tt.assert != nil {
-					tt.assert(t, conn)
+				require.NotEmpty(t, conn)
+				if tt.body != "" {
+					var want *api.Connections
+					require.NoError(t, json.Unmarshal([]byte(tt.body), &want))
+					assert.Equal(t, want, conn)
+				}
+			}
+		})
+	}
+}
+
+func TestCache_Folders(t *testing.T) {
+	tests := []struct {
+		name        string
+		statusCode  int
+		contentType string
+		body        string
+		assertErr   func(t *testing.T, err error)
+	}{
+		{
+			name:        "OK",
+			statusCode:  http.StatusOK,
+			contentType: "application/json",
+			body: `
+[{
+	"id": "default",
+	"label": "Default Folder",
+	"filesystemType": "basic",
+	"path": "/Default Folder",
+	"type": "sendreceive",
+	"devices": [
+		{
+			"deviceID": "XXXXXX1-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXX1",
+			"introducedBy": "",
+			"encryptionPassword": ""
+		}
+	],
+	"rescanIntervalS": 3600,
+	"fsWatcherEnabled": true,
+	"fsWatcherDelayS": 10,
+	"ignorePerms": false,
+	"autoNormalize": true,
+	"minDiskFree": {
+		"value": 0,
+		"unit": ""
+	},
+	"versioning": {
+		"type": "",
+		"params": {},
+		"cleanupIntervalS": 3600,
+		"fsPath": "",
+		"fsType": "basic"
+	},
+	"copiers": 0,
+	"pullerMaxPendingKiB": 0,
+	"hashers": 0,
+	"order": "random",
+	"ignoreDelete": false,
+	"scanProgressIntervalS": 0,
+	"pullerPauseS": 0,
+	"maxConflicts": -1,
+	"disableSparseFiles": false,
+	"disableTempIndexes": false,
+	"paused": false,
+	"weakHashThresholdPct": 25,
+	"markerName": ".stfolder",
+	"copyOwnershipFromParent": false,
+	"modTimeWindowS": 0,
+	"maxConcurrentWrites": 2,
+	"disableFsync": false,
+	"blockPullOrder": "standard",
+	"copyRangeMethod": "standard",
+	"caseSensitiveFS": false,
+	"junctionsAsDirs": false,
+	"syncOwnership": false,
+	"sendOwnership": false,
+	"syncXattrs": false,
+	"sendXattrs": false,
+	"xattrFilter": {
+		"entries": [],
+		"maxSingleEntrySize": 0,
+		"maxTotalSize": 0
+	}
+}]`,
+		},
+		{
+			name:        "empty body",
+			statusCode:  http.StatusOK,
+			contentType: "application/json",
+			assertErr: func(t *testing.T, err error) {
+				var syntaxErr *json.SyntaxError
+				assert.ErrorAs(t, err, &syntaxErr)
+			},
+		},
+		{
+			name:        "has error",
+			statusCode:  http.StatusInternalServerError,
+			contentType: "application/json",
+			body:        `{ "error": "some error message" }`,
+			assertErr: func(t *testing.T, err error) {
+				assert.ErrorContains(t, err,
+					"folders: unexpected syncthing error: some error message")
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			httpClient := testHttpDoer{
+				func(req *http.Request) (*http.Response, error) {
+					r := httptest.NewRecorder()
+					r.Header().Set("Content-Type", tt.contentType)
+					r.WriteHeader(tt.statusCode)
+					_, err := r.WriteString(tt.body)
+					require.NoError(t, err)
+					return r.Result(), nil
+				},
+			}
+
+			c, err := New("/", func(self *Client) error {
+				return self.NewClientWithResponses(api.WithHTTPClient(&httpClient))
+			})
+			require.NoError(t, err)
+			require.NotNil(t, c)
+
+			folders, err := c.Folders(context.Background())
+			if tt.assertErr != nil {
+				t.Log(err)
+				require.Error(t, err)
+				tt.assertErr(t, err)
+			} else {
+				require.NoError(t, err)
+				require.NotEmpty(t, folders)
+				if tt.body != "" {
+					var want []api.FolderConfiguration
+					require.NoError(t, json.Unmarshal([]byte(tt.body), &want))
+					assert.Equal(t, want, folders)
+				}
+			}
+		})
+	}
+}
+
+func TestCache_Devices(t *testing.T) {
+	tests := []struct {
+		name        string
+		statusCode  int
+		contentType string
+		body        string
+		assertErr   func(t *testing.T, err error)
+	}{
+		{
+			name:        "OK",
+			statusCode:  http.StatusOK,
+			contentType: "application/json",
+			body: `
+[
+  {
+    "deviceID": "XXXXXX1-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXX1",
+    "name": "some device",
+    "addresses": [
+      "dynamic"
+    ],
+    "compression": "metadata",
+    "certName": "",
+    "introducer": false,
+    "skipIntroductionRemovals": false,
+    "introducedBy": "",
+    "paused": false,
+    "allowedNetworks": [],
+    "autoAcceptFolders": false,
+    "maxSendKbps": 0,
+    "maxRecvKbps": 0,
+    "ignoredFolders": [],
+    "maxRequestKiB": 0,
+    "untrusted": false,
+    "remoteGUIPort": 0,
+    "numConnections": 0
+  }
+]`,
+		},
+		{
+			name:        "empty body",
+			statusCode:  http.StatusOK,
+			contentType: "application/json",
+			assertErr: func(t *testing.T, err error) {
+				var syntaxErr *json.SyntaxError
+				assert.ErrorAs(t, err, &syntaxErr)
+			},
+		},
+		{
+			name:        "has error",
+			statusCode:  http.StatusInternalServerError,
+			contentType: "application/json",
+			body:        `{ "error": "some error message" }`,
+			assertErr: func(t *testing.T, err error) {
+				assert.ErrorContains(t, err,
+					"devices: unexpected syncthing error: some error message")
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			httpClient := testHttpDoer{
+				func(req *http.Request) (*http.Response, error) {
+					r := httptest.NewRecorder()
+					r.Header().Set("Content-Type", tt.contentType)
+					r.WriteHeader(tt.statusCode)
+					_, err := r.WriteString(tt.body)
+					require.NoError(t, err)
+					return r.Result(), nil
+				},
+			}
+
+			c, err := New("/", func(self *Client) error {
+				return self.NewClientWithResponses(api.WithHTTPClient(&httpClient))
+			})
+			require.NoError(t, err)
+			require.NotNil(t, c)
+
+			devices, err := c.Devices(context.Background())
+			if tt.assertErr != nil {
+				t.Log(err)
+				require.Error(t, err)
+				tt.assertErr(t, err)
+			} else {
+				require.NoError(t, err)
+				require.NotEmpty(t, devices)
+				if tt.body != "" {
+					var want []api.DeviceConfiguration
+					require.NoError(t, json.Unmarshal([]byte(tt.body), &want))
+					assert.Equal(t, want, devices)
+				}
+			}
+		})
+	}
+}
+
+func TestCache_DeviceStats(t *testing.T) {
+	tests := []struct {
+		name        string
+		statusCode  int
+		contentType string
+		body        string
+		assertErr   func(t *testing.T, err error)
+	}{
+		{
+			name:        "OK",
+			statusCode:  http.StatusOK,
+			contentType: "application/json",
+			body: `
+{
+  "XXXXXX1-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXX1": {
+    "lastSeen": "2024-03-28T20:15:11+01:00",
+    "lastConnectionDurationS": 451.813963185
+  }
+}`,
+		},
+		{
+			name:        "empty body",
+			statusCode:  http.StatusOK,
+			contentType: "application/json",
+			assertErr: func(t *testing.T, err error) {
+				var syntaxErr *json.SyntaxError
+				assert.ErrorAs(t, err, &syntaxErr)
+			},
+		},
+		{
+			name:        "has error",
+			statusCode:  http.StatusInternalServerError,
+			contentType: "application/json",
+			body:        `{ "error": "some error message" }`,
+			assertErr: func(t *testing.T, err error) {
+				assert.ErrorContains(t, err,
+					"device stats: unexpected syncthing error: some error message")
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			httpClient := testHttpDoer{
+				func(req *http.Request) (*http.Response, error) {
+					r := httptest.NewRecorder()
+					r.Header().Set("Content-Type", tt.contentType)
+					r.WriteHeader(tt.statusCode)
+					_, err := r.WriteString(tt.body)
+					require.NoError(t, err)
+					return r.Result(), nil
+				},
+			}
+
+			c, err := New("/", func(self *Client) error {
+				return self.NewClientWithResponses(api.WithHTTPClient(&httpClient))
+			})
+			require.NoError(t, err)
+			require.NotNil(t, c)
+
+			stats, err := c.DeviceStats(context.Background())
+			if tt.assertErr != nil {
+				t.Log(err)
+				require.Error(t, err)
+				tt.assertErr(t, err)
+			} else {
+				require.NoError(t, err)
+				require.NotEmpty(t, stats)
+				if tt.body != "" {
+					var want map[string]api.DeviceStatistics
+					require.NoError(t, json.Unmarshal([]byte(tt.body), &want))
+					assert.Equal(t, want, stats)
+				}
+			}
+		})
+	}
+}
+
+func TestCache_Completion(t *testing.T) {
+	const folder = "default"
+	const device = "XXXXXX1-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXX1"
+
+	tests := []struct {
+		name           string
+		folder, device string
+		statusCode     int
+		contentType    string
+		body           string
+		assertErr      func(t *testing.T, err error)
+	}{
+		{
+			name:        "OK",
+			statusCode:  http.StatusOK,
+			contentType: "application/json",
+			body: `
+{
+  "completion": 100,
+  "globalBytes": 10574239170,
+  "globalItems": 2808,
+  "needBytes": 0,
+  "needDeletes": 0,
+  "needItems": 0,
+  "remoteState": "unknown",
+  "sequence": 0
+}`,
+		},
+		{
+			name:        "OK with folder",
+			folder:      folder,
+			statusCode:  http.StatusOK,
+			contentType: "application/json",
+			body: `
+{
+  "completion": 100,
+  "globalBytes": 26996726,
+  "globalItems": 39,
+  "needBytes": 0,
+  "needDeletes": 0,
+  "needItems": 0,
+  "remoteState": "unknown",
+  "sequence": 145
+}`,
+		},
+		{
+			name:        "OK with folder and device",
+			folder:      folder,
+			device:      device,
+			statusCode:  http.StatusOK,
+			contentType: "application/json",
+			body: `
+{
+  "completion": 100,
+  "globalBytes": 26996726,
+  "globalItems": 39,
+  "needBytes": 0,
+  "needDeletes": 0,
+  "needItems": 0,
+  "remoteState": "valid",
+  "sequence": 805
+}`,
+		},
+		{
+			name:        "OK device",
+			device:      device,
+			statusCode:  http.StatusOK,
+			contentType: "application/json",
+			body: `
+{
+  "completion": 100,
+  "globalBytes": 567088438,
+  "globalItems": 1003,
+  "needBytes": 0,
+  "needDeletes": 0,
+  "needItems": 0,
+  "remoteState": "unknown",
+  "sequence": 0
+}`,
+		},
+		{
+			name:        "empty body",
+			statusCode:  http.StatusOK,
+			contentType: "application/json",
+			assertErr: func(t *testing.T, err error) {
+				var syntaxErr *json.SyntaxError
+				assert.ErrorAs(t, err, &syntaxErr)
+			},
+		},
+		{
+			name:        "has error",
+			statusCode:  http.StatusInternalServerError,
+			contentType: "application/json",
+			body:        `{ "error": "some error message" }`,
+			assertErr: func(t *testing.T, err error) {
+				assert.ErrorContains(t, err,
+					"completion: unexpected syncthing error: some error message")
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			httpClient := testHttpDoer{
+				func(req *http.Request) (*http.Response, error) {
+					r := httptest.NewRecorder()
+					r.Header().Set("Content-Type", tt.contentType)
+					r.WriteHeader(tt.statusCode)
+					_, err := r.WriteString(tt.body)
+					require.NoError(t, err)
+					return r.Result(), nil
+				},
+			}
+
+			c, err := New("/", func(self *Client) error {
+				return self.NewClientWithResponses(api.WithHTTPClient(&httpClient))
+			})
+			require.NoError(t, err)
+			require.NotNil(t, c)
+
+			comp, err := c.Completion(context.Background(), tt.folder, tt.device)
+			if tt.assertErr != nil {
+				t.Log(err)
+				require.Error(t, err)
+				tt.assertErr(t, err)
+			} else {
+				require.NoError(t, err)
+				require.NotEmpty(t, comp)
+				if tt.body != "" {
+					var want api.FolderCompletion
+					require.NoError(t, json.Unmarshal([]byte(tt.body), &want))
+					assert.Equal(t, &want, comp)
+				}
+			}
+		})
+	}
+}
+
+func TestCache_SystemErrors(t *testing.T) {
+	tests := []struct {
+		name           string
+		folder, device string
+		statusCode     int
+		contentType    string
+		body           string
+		assertErr      func(t *testing.T, err error)
+	}{
+		{
+			name:        "OK no system errors",
+			statusCode:  http.StatusOK,
+			contentType: "application/json",
+			body: `
+{
+  "errors": null
+}`,
+		},
+		{
+			name:        "OK with system errors",
+			statusCode:  http.StatusOK,
+			contentType: "application/json",
+			body: `
+{
+  "errors": [
+    {
+      "level": 0,
+      "message": "some error",
+      "when": "2024-03-28T20:15:11+01:00"
+    }
+  ]
+}`,
+		},
+		{
+			name:        "empty body",
+			statusCode:  http.StatusOK,
+			contentType: "application/json",
+			assertErr: func(t *testing.T, err error) {
+				var syntaxErr *json.SyntaxError
+				assert.ErrorAs(t, err, &syntaxErr)
+			},
+		},
+		{
+			name:        "has error",
+			statusCode:  http.StatusInternalServerError,
+			contentType: "application/json",
+			body:        `{ "error": "some error message" }`,
+			assertErr: func(t *testing.T, err error) {
+				assert.ErrorContains(t, err,
+					"system errors: unexpected syncthing error: some error message")
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			httpClient := testHttpDoer{
+				func(req *http.Request) (*http.Response, error) {
+					r := httptest.NewRecorder()
+					r.Header().Set("Content-Type", tt.contentType)
+					r.WriteHeader(tt.statusCode)
+					_, err := r.WriteString(tt.body)
+					require.NoError(t, err)
+					return r.Result(), nil
+				},
+			}
+
+			c, err := New("/", func(self *Client) error {
+				return self.NewClientWithResponses(api.WithHTTPClient(&httpClient))
+			})
+			require.NoError(t, err)
+			require.NotNil(t, c)
+
+			sysErrors, err := c.SystemErrors(context.Background())
+			if tt.assertErr != nil {
+				t.Log(err)
+				require.Error(t, err)
+				tt.assertErr(t, err)
+			} else {
+				require.NoError(t, err)
+				if tt.body != "" {
+					var want api.SystemErrors
+					require.NoError(t, json.Unmarshal([]byte(tt.body), &want))
+					assert.Equal(t, want.Errors, sysErrors)
+				}
+			}
+		})
+	}
+}
+
+func TestCache_FolderErrors(t *testing.T) {
+	tests := []struct {
+		name           string
+		folder, device string
+		statusCode     int
+		contentType    string
+		body           string
+		assertErr      func(t *testing.T, err error)
+	}{
+		{
+			name:        "OK no folder errors",
+			statusCode:  http.StatusOK,
+			contentType: "application/json",
+			body: `
+{
+  "errors": null,
+  "folder": "default",
+  "page": 1,
+  "perpage": 65536
+}`,
+		},
+		{
+			name:        "OK with folder errors",
+			statusCode:  http.StatusOK,
+			contentType: "application/json",
+			body: `
+{
+  "errors": [
+    {
+      "error": "some error",
+      "path": "/some file path"
+    }
+  ],
+  "folder": "default",
+  "page": 1,
+  "perpage": 65536
+}`,
+		},
+		{
+			name:        "empty body",
+			statusCode:  http.StatusOK,
+			contentType: "application/json",
+			assertErr: func(t *testing.T, err error) {
+				var syntaxErr *json.SyntaxError
+				assert.ErrorAs(t, err, &syntaxErr)
+			},
+		},
+		{
+			name:        "has error",
+			statusCode:  http.StatusInternalServerError,
+			contentType: "application/json",
+			body:        `{ "error": "some error message" }`,
+			assertErr: func(t *testing.T, err error) {
+				assert.ErrorContains(t, err,
+					"folder errors: unexpected syncthing error: some error message")
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			httpClient := testHttpDoer{
+				func(req *http.Request) (*http.Response, error) {
+					r := httptest.NewRecorder()
+					r.Header().Set("Content-Type", tt.contentType)
+					r.WriteHeader(tt.statusCode)
+					_, err := r.WriteString(tt.body)
+					require.NoError(t, err)
+					return r.Result(), nil
+				},
+			}
+
+			c, err := New("/", func(self *Client) error {
+				return self.NewClientWithResponses(api.WithHTTPClient(&httpClient))
+			})
+			require.NoError(t, err)
+			require.NotNil(t, c)
+
+			folderErrors, err := c.FolderErrors(context.Background(), "default")
+			if tt.assertErr != nil {
+				t.Log(err)
+				require.Error(t, err)
+				tt.assertErr(t, err)
+			} else {
+				require.NoError(t, err)
+				if tt.body != "" {
+					var want api.FolderErrors
+					require.NoError(t, json.Unmarshal([]byte(tt.body), &want))
+					assert.Equal(t, want.Errors, folderErrors)
 				}
 			}
 		})
